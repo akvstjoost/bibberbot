af7e699597d9cfa091697ef39a32f468d509359c
Thomasrutgers <thomasrutgers@gmail.com>
2018-03-25 23:48:39 +0200

    Changed for mpu6050

--- bibberbot.ino
+++ bibberbot.ino
@@ -1,7 +1,11 @@
 #include <Wire.h>
-#include <LSM6.h>
+#include "I2Cdev.h"
+#include "MPU6050.h"
 
-LSM6 imu;
+MPU6050 accelgyro;
+
+int16_t ax, ay, az;
+int16_t gx, gy, gz;
 
 unsigned long stepperStamp;
 unsigned long rightStamp;
@@ -15,26 +19,23 @@ int rightDirPin = 9;
 unsigned long gyroInterval = 5000;
 
 //speed->interval conversion
-float speedDeadzone = 0.5; //motor only starts working at this speed
-unsigned long intervalAt100 = 1000;
-unsigned long minInterval  = 1000;
-unsigned long maxInterval  = 10000;
+float speedDeadzone = 0; //motor only starts working at this speed
+unsigned long intervalAt100 = 1200;
+unsigned long minInterval  = 1500;
+unsigned long maxInterval  = 500000;
 float exponentiality = 0; //between 0 (linear) and 1 (semi-exponential)
 
 //gyroscope
 float previousAngle = 0;
-float filterCoeff = 0.996;
+float filterCoeff = 0.99;
 //control loop
-float targetAngle = -1.5;
+float targetAngle = -10;
 float errorSum = 0;
-float Kp = 5;
-float Ki = 0.1;
-float Kd = 0.2;
-//3.3,0.07,0.02valmost
-//4.2 0.09 0.02
-float lastls = 0;
-float lastrs = 0;
-
+float Kp = 4;
+float Ki = 0.4;
+float Kd = 0.1;
+//8 0.27 0.03
+//6 0.3 0.03
 void setup() {
   Serial.begin(9600);
   pinMode(leftStepPin, OUTPUT);
@@ -42,8 +43,10 @@ void setup() {
   pinMode(leftDirPin, OUTPUT);
   pinMode(rightDirPin, OUTPUT);
   Wire.begin();
-  if (!imu.init()) Serial.println("Failed to detect and initialize IMU!");
-  imu.enableDefault();
+  accelgyro.initialize();
+  Serial.println(accelgyro.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
+
+
   //plotGraph();
 }
 
@@ -52,15 +55,15 @@ void loop() {
 }
 
 void setInterval(float s) {
-  if (s > 0) {
+  if (s < 0) {
     digitalWrite(leftDirPin, LOW);
     digitalWrite(rightDirPin, HIGH);
-      }
-  if (s < 0) { 
+  }
+  if (s > 0) {
     digitalWrite(leftDirPin, HIGH);
     digitalWrite(rightDirPin, LOW);
   }
-  s = abs(min(s,100));
+  s = abs(min(s, 100));
   if (s <= speedDeadzone) stepperOn = false;
   else {
     stepperOn = true;
@@ -70,9 +73,9 @@ void setInterval(float s) {
            map(s, 0.0, 100.0, maxInterval, intervalAt100)
          )) +
         ((1.0 - exponentiality) * ( //a 1/x speed interval formula gives linear speed control
-           (intervalAt100*100) / s)
+           (intervalAt100 * 100) / s)
         ),
-      minInterval, maxInterval);
+        minInterval, maxInterval) / 2;
   }
 }
 
@@ -87,10 +90,9 @@ void checkMicros() {
   }
   if (currentMicros > gyroStamp + gyroInterval) {
     unsigned long loopTime = (currentMicros - gyroStamp) / 1000;
-    imu.read();
-    float accAngle = 0 - atan2(imu.a.z, 0 - imu.a.x) * RAD_TO_DEG;
-    int gyroX = imu.g.y;
-    int gyroRate = map(gyroX + 897, -32768, 32767, -290, 290);
+    accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+    float accAngle =  atan2(az, ax) * RAD_TO_DEG;
+    int gyroRate = map(gy - 10, -32768, 32768, -250, 250);
     float gyroAngle = (float)gyroRate * loopTime / 1000;
     float currentAngle = filterCoeff * (previousAngle + gyroAngle) + ( (1 - filterCoeff) * accAngle);
     float error = currentAngle - targetAngle;
@@ -100,14 +102,19 @@ void checkMicros() {
     setInterval(motorPower);
     previousAngle = currentAngle;
     gyroStamp = currentMicros;
+    //if (abs(motorPower) < 3) {
+    //  targetAngle = (0.99 * targetAngle) + (0.01 * currentAngle);
+   // }
+   //Serial.println(currentAngle);
   }
 }
 
 void plotGraph() {
-  for (int x=0;x<=100;x++) {
-  setInterval(x);
-  Serial.println(stepperInterval);
-  delay(10);
+  for (int x = 0; x <= 100; x++) {
+    setInterval(x);
+    Serial.println(stepperInterval);
+    delay(10);
   }
 }
+
 
