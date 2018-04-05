20a45f5f229e9407b60f40a2455c7d6c3c097c81
Thomasrutgers <thomasrutgers@gmail.com>
2018-04-05 19:12:16 +0200

    Create bibberbotprimo.ino

---
+++ bibberbotprimo.ino
@@ -0,0 +1,175 @@
+#include <Wire.h>
+#include "I2Cdev.h"
+#include "MPU6050.h"
+#include <WiFiLink.h>
+#include <PubSubClient.h>
+#include "secret.h";
+#include <stdlib.h>
+
+
+//stepper
+unsigned long stepperStamp;
+unsigned long rightStamp;
+unsigned long gyroStamp;
+unsigned long stepperInterval = 5000;
+boolean stepperOn = false;
+int leftStepPin = 2;
+int leftDirPin = 3;
+int rightStepPin = 4;
+int rightDirPin = 5;
+unsigned long gyroInterval = 5000;
+
+//speed->interval conversion
+float speedDeadzone = 0; //motor only starts working at this speed
+unsigned long intervalAt100 = 1200;
+unsigned long minInterval  = 1500;
+unsigned long maxInterval  = 500000;
+
+//gyroscope
+MPU6050 accelgyro;
+float previousAngle = 0;
+float filterCoeff = 0.99;
+//control loop
+float targetAngle = -10;
+float errorSum = 0;
+float Kp = 4;
+float Ki = 0.4;
+float Kd = 0.1;
+//8 0.27 0.03
+//6 0.3 0.03
+
+//controller
+int controllerSpeed = 0;
+int controllerTurn = 0;
+
+//mqtt
+const char* mqtt_server = "broker.mqtt-dashboard.com";
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastMsg = 0;
+long lastReconnectAttempt = 0;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(leftStepPin, OUTPUT);
+  pinMode(rightStepPin, OUTPUT);
+  pinMode(leftDirPin, OUTPUT);
+  pinMode(rightDirPin, OUTPUT);
+  Wire.begin();
+  accelgyro.initialize();
+  Serial.println(accelgyro.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+}
+
+void loop() {
+  checkMicros();
+  mqttloop();
+}
+
+void setInterval(float s) {
+  if (s < 0) {
+    digitalWrite(leftDirPin, LOW);
+    digitalWrite(rightDirPin, HIGH);
+  }
+  if (s > 0) {
+    digitalWrite(leftDirPin, HIGH);
+    digitalWrite(rightDirPin, LOW);
+  }
+  s = abs(min(s, 100));
+  if (s <= speedDeadzone) stepperOn = false;
+  else {
+    stepperOn = true;
+    stepperInterval =
+      constrain((intervalAt100 * 100) / s, minInterval, maxInterval) / 2;
+  }
+}
+
+void checkMicros() {
+  unsigned long currentMicros = micros();
+  if (stepperOn and currentMicros > stepperStamp + stepperInterval) {
+    digitalWrite(leftStepPin, HIGH);
+    digitalWrite(leftStepPin, LOW);
+    digitalWrite(rightStepPin, HIGH);
+    digitalWrite(rightStepPin, LOW);
+    stepperStamp = stepperStamp + stepperInterval;
+  }
+  if (currentMicros > gyroStamp + gyroInterval) {
+    int16_t ax, ay, az, gx, gy, gz;
+    unsigned long loopTime = (currentMicros - gyroStamp) / 1000;
+    accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+    float accAngle =  atan2(az, ax) * RAD_TO_DEG;
+    int gyroRate = map(gy - 10, -32768, 32768, -250, 250);
+    float gyroAngle = (float)gyroRate * loopTime / 1000;
+    float currentAngle = filterCoeff * (previousAngle + gyroAngle) + ( (1 - filterCoeff) * accAngle);
+    float error = currentAngle - targetAngle;
+    errorSum = errorSum + error;
+    errorSum = constrain(errorSum, -300, 300);
+    float motorPower = Kp * (error) + Ki * (errorSum) * loopTime - Kd * (currentAngle - previousAngle) / loopTime;
+    setInterval(motorPower);
+    previousAngle = currentAngle;
+    gyroStamp = currentMicros;
+  }
+}
+
+void setup_wifi() {
+  delay(10);
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(NETWORK);
+  WiFi.begin(NETWORK, PASS);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  char val[length + 1];
+  for (int i = 0; i < length; i++) {
+    val[i] = (char)payload[i];
+  }
+  val[length + 1] = '\0';
+  if (!strcmp(topic, "bibberBot/speed")) controllerSpeed = atoi(val);
+  if (!strcmp(topic, "bibberBot/turn")) controllerTurn = atoi(val);
+}
+
+
+boolean reconnect() {
+  if (client.connect("bibberBot")) {
+    Serial.println("connected");
+    client.subscribe("bibberBot/speed");
+    client.subscribe("bibberBot/turn");
+  }
+  return client.connected();
+}
+
+void mqttloop() {
+  long now = millis();
+  if (!client.connected()) {
+    if (now - lastReconnectAttempt > 5000) {
+      lastReconnectAttempt = now;
+      // Attempt to reconnect
+      if (reconnect()) {
+        lastReconnectAttempt = 0;
+      }
+    }
+  } else {
+
+    client.loop();
+    /*
+    if (now - lastMsg > 2000) {
+      lastMsg = now;
+
+      client.publish("outTopic", "hello world");
+    }
+    */
+  }
+
+}
