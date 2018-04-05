ff9f27e6d67e9368d96f5355b778c9496cb39c37
Thomasrutgers <thomasrutgers@gmail.com>
2018-04-05 14:54:45 +0200

    Update mqtt.ino

--- test/mqtt.ino
+++ test/mqtt.ino
@@ -8,11 +8,13 @@ const char* mqtt_server = "broker.mqtt-dashboard.com";
 WiFiClient espClient;
 PubSubClient client(espClient);
 long lastMsg = 0;
+long lastReconnectAttempt = 0;
 char msg[50];
 int value = 0;
 
 void setup() {
   pinMode(9, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
+  pinMode(38, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
   Serial.begin(115200);
   setup_wifi();
   client.setServer(mqtt_server, 1883);
@@ -24,8 +26,8 @@ void setup_wifi() {
   Serial.println();
   Serial.print("Connecting to ");
   Serial.println(NETWORK);
-  WiFi.begin(NETWORK,PASS);
-while (WiFi.status() != WL_CONNECTED) {
+  WiFi.begin(NETWORK, PASS);
+  while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
@@ -37,47 +39,50 @@ while (WiFi.status() != WL_CONNECTED) {
 }
 
 void callback(char* topic, byte* payload, unsigned int length) {
-  char val[length+1];
-  for (int i=0;i<length;i++) {
+  char val[length + 1];
+  for (int i = 0; i < length; i++) {
     val[i] = (char)payload[i];
   }
-  val[length+1] = '\0';
+  val[length + 1] = '\0';
   if (!strcmp(topic, "bibberBot/speed")) Serial.println("speed:");
   if (!strcmp(topic, "bibberBot/turn")) Serial.println("turn:");
   Serial.println(atoi(val));
 }
 
-void reconnect() {
-  // Loop until we're reconnected
-  while (!client.connected()) {
-    Serial.print("Attempting MQTT connection...");
-    if (client.connect("Bibberbot")) {
-      Serial.println("connected");
-      client.publish("thomasOut", "hello world");
-      client.subscribe("bibberBot/speed");
-      client.subscribe("bibberBot/turn");
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
-      delay(5000);
+
+boolean reconnect() {
+  if (client.connect("bibberBot")) {
+    Serial.println("connected");
+    client.publish("thomasOut", "hello world");
+    client.subscribe("bibberBot/speed");
+    client.subscribe("bibberBot/turn");
+  }
+  return client.connected();
+}
+
+
+void loop() {
+  long now = millis();
+  if (!client.connected()) {
+    if (now - lastReconnectAttempt > 5000) {
+      lastReconnectAttempt = now;
+      // Attempt to reconnect
+      if (reconnect()) {
+        lastReconnectAttempt = 0;
+      }
+    }
+  } else {
+    // Client connected
+
+    client.loop();
+    if (now - lastMsg > 2000) {
+      lastMsg = now;
+      ++value;
+      snprintf (msg, 75, "hello world #%ld", value);
+      Serial.print("Publish message: ");
+      Serial.println(msg);
+      client.publish("outTopic", msg);
     }
   }
-}
-void loop() {
 
-  if (!client.connected()) {
-    reconnect();
-  }
-  client.loop();
-  long now = millis();
-  if (now - lastMsg > 2000) {
-    lastMsg = now;
-    ++value;
-    snprintf (msg, 75, "hello world #%ld", value);
-    Serial.print("Publish message: ");
-    Serial.println(msg);
-    client.publish("outTopic", msg);
-  }
 }
